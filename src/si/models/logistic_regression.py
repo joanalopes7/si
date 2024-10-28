2b63c3296703f2e136336c4bc1060079c6ffb84b
joanalopes0711 <153217628+joanalopes0711@users.noreply.github.com>
2024-10-28 09:25:18 +0000

    [UPDATE]

--- src/si/models/logistic_regression.py
+++ src/si/models/logistic_regression.py
@@ -2,16 +2,11 @@ import numpy as np
 
 from si.base.model import Model
 from si.data.dataset import Dataset
-<<<<<<< HEAD
-=======
 from si.metrics.accuracy import accuracy
->>>>>>> 798823c15b5d67e400b65f8e9be43ee1e13774e5
 from si.statistics.sigmoid_function import sigmoid_function
 
 
 class LogisticRegression(Model):
-<<<<<<< HEAD
-=======
     """
     The LogisticRegression is a logistic model using the L2 regularization.
     This model solves the logistic regression problem using an adapted Gradient Descent technique
@@ -35,7 +30,6 @@ class LogisticRegression(Model):
     """
     def __init__(self, l2_penalty: float = 1, alpha: float = 0.001, max_iter: int = 1000, patience: int = 5,
                  scale: bool = True, **kwargs):
->>>>>>> 798823c15b5d67e400b65f8e9be43ee1e13774e5
         """
 
         Parameters
@@ -51,99 +45,6 @@ class LogisticRegression(Model):
         scale: bool
             Whether to scale the dataset or not
         """
-<<<<<<< HEAD
-
-        def __init__(self, l2_penalty: float = 1, alpha: float = 0.001, max_iter: int = 1000, patience: int = 5,
-                 scale: bool = True, **kwargs):
-        
-            # parameters
-            super().__init__(**kwargs)
-            self.l2_penalty = l2_penalty
-            self.alpha = alpha
-            self.max_iter = max_iter
-            self.patience = patience
-            self.scale = scale
-
-            # attributes
-            self.theta = None
-            self.theta_zero = None
-            self.mean = None
-            self.std = None
-            self.cost_history = {}
-
-        def _fit(self, dataset: Dataset) -> 'LogisticRegression':
-            """
-            Fit the model to the dataset
-
-            Parameters
-            ----------
-            dataset: Dataset
-                The dataset to fit the model to
-
-            Returns
-            -------
-            self: LogisticRegression
-                The fitted model
-            """
-            if self.scale:
-                # compute mean and std
-                self.mean = np.nanmean(dataset.X, axis=0)
-                self.std = np.nanstd(dataset.X, axis=0)
-                # scale the dataset
-                X = (dataset.X - self.mean) / self.std
-            else:
-                X = dataset.X
-
-            m, n = dataset.shape()
-
-            # initialize the model parameters
-            self.theta = np.zeros(n)
-            self.theta_zero = 0
-
-            i = 0
-            early_stopping = 0
-            # gradient descent
-            while i < self.max_iter and early_stopping < self.patience:
-                # predicted y
-                y_pred = sigmoid_function(np.dot(X, self.theta) + self.theta_zero)
-
-                # computing and updating the gradient with the learning rate
-                gradient = (self.alpha / m) * np.dot(y_pred - dataset.y, X)
-
-                # computing the penalty
-                penalization_term = self.theta * (1 - self.alpha * (self.l2_penalty / m))
-
-                # updating the model parameters
-                self.theta = penalization_term - gradient
-                self.theta_zero = self.theta_zero - (self.alpha * (1 / m)) * np.sum(y_pred - dataset.y)
-
-                # compute the cost
-                self.cost_history[i] = self.cost(dataset)
-                if i > 0 and self.cost_history[i] > self.cost_history[i - 1]:
-                    early_stopping += 1
-                else:
-                    early_stopping = 0
-                i += 1
-
-            return self
-        
-        def _predict(self, dataset: Dataset) -> np.array:
-            """
-            Predict the output of the dataset
-
-            Parameters
-            ----------
-            dataset: Dataset
-                The dataset to predict the output of
-
-            Returns
-            -------
-            predictions: np.array
-                The predictions of the dataset
-            """
-            X = (dataset.X - self.mean) / self.std if self.scale else dataset.X
-            prob_values=sigmoid_function(np.dot(X, self.theta) + self.theta_zero)
-=======
         # parameters
         super().__init__(**kwargs)
         self.l2_penalty = l2_penalty
@@ -325,5 +226,4 @@ if __name__ == '__main__':
 
     plt.plot(list(model.cost_history.keys()), list(model.cost_history.values()))
     plt.show()
->>>>>>> 798823c15b5d67e400b65f8e9be43ee1e13774e5
 
