0d6a2c74b6d20d108c58c216a237948d82d7975b
joanalopes0711 <153217628+joanalopes0711@users.noreply.github.com>
2024-11-22 15:01:03 +0000

    [update sigmoide_function]

--- src/si/statistics/sigmoid_function.py
+++ src/si/statistics/sigmoid_function.py
@@ -1,19 +1,5 @@
 import numpy as np
 
-<<<<<<< HEAD
-def sigmoid_function (x:np.array) -> float :
-    """
-    Calculate the sigmoid function for the input values.
-
-    Parameters:
-    X(array): Input values.
-
-    Returns:
-    array: The sigmoid of the input values.
-    """
-    
-    return 1 / (1 + np.exp(-x))
-=======
 
 def sigmoid_function(X: np.ndarray) -> np.ndarray:
     """
@@ -30,4 +16,3 @@ def sigmoid_function(X: np.ndarray) -> np.ndarray:
         The sigmoid function of the given input
     """
     return 1 / (1 + np.exp(-X))
->>>>>>> 798823c15b5d67e400b65f8e9be43ee1e13774e5
