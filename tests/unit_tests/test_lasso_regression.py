d18d3912bf592e7c1b9ce8692fd0d40b53df81b1
joanalopes0711 <153217628+joanalopes0711@users.noreply.github.com>
2024-12-17 16:35:12 +0000

    [update] test test_lasso_regression

--- tests/unit_tests/test_lasso_regression.py
+++ tests/unit_tests/test_lasso_regression.py
@@ -1,13 +1,11 @@
 from unittest import TestCase
-
 import numpy as np
 from datasets import DATASETS_PATH
-
 import os
 from si.io.csv_file import read_csv
-
 from si.model_selection.split import train_test_split
 from si.models.lasso_regression import LassoRegression
+
 
 class TestLassoRegressor(TestCase):
 
@@ -20,21 +18,21 @@ class TestLassoRegressor(TestCase):
 
     def test_fit(self):
 
-        ridge = LassoRegression()
-        ridge.fit(self.train_dataset)
+        lasso = LassoRegression()
+        lasso.fit(self.train_dataset)
 
 
     def test_predict(self):
-        ridge = LassoRegression()
-        ridge.fit(self.train_dataset)
+        lasso = LassoRegression()
+        lasso.fit(self.train_dataset)
 
-        predictions = ridge.predict(self.test_dataset)
+        predictions = lasso.predict(self.test_dataset)
 
         self.assertEqual(predictions.shape[0], self.test_dataset.shape()[0])
     
     def test_score(self):
-        ridge = LassoRegression(scale=True, patience=5)
-        ridge.fit(self.train_dataset)
-        mse_ = ridge.score(self.test_dataset)
+        lasso = LassoRegression(scale=True, patience=5)
+        lasso.fit(self.train_dataset)
+        mse_ = lasso.score(self.test_dataset)
 
-        self.assertEqual(round(mse_, 2), 5777.56)
+        self.assertEqual(round(mse_, 1), 5777.5)
