c0c3f790f1918a9638051adfb80947feb9a964e1
joanalopes0711 <153217628+joanalopes0711@users.noreply.github.com>
2025-01-02 18:24:04 +0000

    [UPDATE] revised

--- src/si/model_selection/split.py
+++ src/si/model_selection/split.py
@@ -62,25 +62,31 @@ def stratified_train_test_split(dataset: Dataset, test_size: float = 0.2, random
     test: Dataset
         The testing dataset
     """
-    # set random state
+    #Set random state
     np.random.seed(random_state)
-    # Get unique class labels and their counts
+
+    #Get unique class labels and their counts
     labels, counts = np.unique(dataset.y, return_counts=True)
-    # Initialize empty lists for training and testing indices
+
+    #Initialize empty lists for training and testing indices
     train_indices = []
     test_indices = []
-     # Loop through unique labels
+
+    #Loop through unique labels
     for label, count in zip(labels, counts):
-        # Get the indices of the examples that belong to the current label
+
+        #Get the indices of the examples that belong to the current label
         label_indices = np.where(dataset.y == label)[0]
-        # Shuffle the indices
+        #Shuffle the indices
         np.random.shuffle(label_indices)
-        # Calculate the number of test samples for the current label
+        #Calculate the number of test samples for the current label
         n_test_samples = int(np.floor(test_size * count))
-        # Select test and training indices for the current label
+        #Select test and training indices for the current label
         test_indices.extend(label_indices[:n_test_samples])
         train_indices.extend(label_indices[n_test_samples:])
-        #get the stratified training and testing datasets
+        #Get the stratified training and testing datasets
+
     train_dataset = Dataset(dataset.X[train_indices], dataset.y[train_indices], features=dataset.features, label=dataset.label)
     test_dataset = Dataset(dataset.X[test_indices], dataset.y[test_indices], features=dataset.features, label=dataset.label)
+    
     return train_dataset, test_dataset
